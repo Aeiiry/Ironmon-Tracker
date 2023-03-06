df090ea38cfb681d00d6b59fec00a4bf22f9deab
Aeiry <micahnbrad@gmail.com>
2023-03-06 14:08:32 +1100

    Adjust chevron size for log, remove duplicate broken code

--- ironmon_tracker/screens/LogOverlay.lua
+++ ironmon_tracker/screens/LogOverlay.lua
@@ -322,6 +322,18 @@ function LogOverlay.addHeaderTabButtons()
 						Drawing.drawImage(icon.image, adjustedX, adjustedY)
 						x = x + (icon.w or 0) + spacer
 					end
+					local prefferredCategory = ""
+					local atkSpaDiff = data.p.atk / data.p.spa
+					if atkSpaDiff > 1.5 then
+						prefferredCategory = "Physical"
+					elseif atkSpaDiff < 0.66 then
+						prefferredCategory = "Special"
+					end
+					if prefferredCategory == "Physical" and MoveData.Moves[self.moveId].category == MoveData.Categories.PHYSICAL then
+						Drawing.drawChevron(self.box[1] + moveCategoryOffset + 8, self.box[2]-1, 2, 1, 1, "up", true)
+					elseif prefferredCategory == "Special" and MoveData.Moves[self.moveId].category == MoveData.Categories.SPECIAL then
+						Drawing.drawChevron(self.box[1] + moveCategoryOffset + 8, self.box[2]-1, 2, 1, 1, "up", true)
+					end
 				end
 				-- if self.isSelected then
 				-- 	Drawing.drawUnderline(self, Theme.COLORS[self.textColor])
